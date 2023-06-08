1e20452f9503a3c739457b5be3a6b530196f74fc
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-06-08 16:31:31 +0300

    Added solution

---
+++ Programs/01_lecture_problem_no_1_LT.py
@@ -0,0 +1,9 @@
+a = float(input("Įveskite skaičių a: "))
+b = float(input("Įveskite skaičių b: "))
+
+if a < b:
+    print("a mažesnis už b")
+elif a == b:
+    print("a lygu b")
+else:
+    print("a didesnis už b")
