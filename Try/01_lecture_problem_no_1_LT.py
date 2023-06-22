038c4ffd2e04f18ea4d9d57275d4754ea257e847
Aurimas Aleksandras Nausėdas <aurimas.nausedas@gmail.com>
2023-06-22 22:23:54 +0300

    Delete Try directory

--- Try/01_lecture_problem_no_1_LT.py
+++
@@ -1,9 +0,0 @@
-a = float(input("Įveskite skaičių a: "))
-b = float(input("Įveskite skaičių b: "))
-
-if a < b:
-    print("a mažesnis už b")
-elif a == b:
-    print("a lygu b")
-else:
-    print("a didesnis už b")
