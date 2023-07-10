272ff15131f24e108fa746af05e8817d6c151378
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-07-10 19:31:36 +0300

    Updated file

---
+++ Programs/modules/kursas.py
@@ -0,0 +1,9 @@
+# kursas.py
+class Kursas:
+    def __init__(self, pavadinimas, destytojas, trukme):
+        self.pavadinimas = pavadinimas
+        self.destytojas = destytojas
+        self.trukme = trukme
+
+    def destyti(self):
+        print("Vyksta mokymas!")
