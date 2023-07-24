756ba8195917bf9a6c6f809dc561d5c26281498f
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-07-24 18:15:47 +0300

    Added file

--- Programs/09_lecture_problem_no_1_LT.py
+++ Programs/09_lecture_problem_no_1_LT.py
@@ -1,13 +1,23 @@
-# Define the input text. You can replace it with your own text.
-input_text = "Labas. Kaip sekasi? Vakar buvo lietus. Šiandien yra graži diena."
+# Importuoti modulį datetime.
+import datetime
 
-# Split the input text into sentences by splitting on periods, exclamation marks, and question marks.
-# For this, replace all exclamation marks and question marks with periods and then split on periods.
-sentences = input_text.replace('!', '.').replace('?', '.').split('.')
+# Atsispausdinti šiandienos datą ir laiką kartu.
+print(datetime.datetime.now())
 
-# Use list comprehension to create a new list of sentences with an exclamation mark added at the end of each sentence.
-# We also strip any leading/trailing whitespace from each sentence.
-new_sentences = [sentence.strip() + "!" for sentence in sentences if sentence.strip()]
+# Atsispausdinti tik datą.
+print(datetime.date.today())
 
-# Combine the modified sentences into a single string with spaces in between using " ".join() and print the result.
-print(" ".join(new_sentences))
+# Atsispausdinti tik laiką.
+print(datetime.datetime.now().time())
+
+# Iš paketo datetime importuoti modulį date.
+from datetime import date
+
+# Atsispausdinti šiandienos datą.
+print(date.today())
+
+# Iš paketo datetime importuoti modulį date kaip data.
+from datetime import date as data
+
+# Atsispausdinti šiandienos datą.
+print(data.today())
