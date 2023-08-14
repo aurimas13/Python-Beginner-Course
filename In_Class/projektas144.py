b71f365617ca0f34d2d370c4087c265d370ffe38
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-08-14 17:40:02 +0300

    Added file

--- In_Class/projektas144.py
+++ In_Class/projektas144.py
@@ -1,7 +1,7 @@
 from projektas141 import Projektas
 from projektas142 import session
 
-search = session.query(Projektas).filter(Projektas.name.ilike('1%'))
+search = session.query(Projektas).filter(Projektas.name.ilike('P%'))
 search2 = session.query(Projektas).filter(Projektas.price > 10000)
 search3 = session.query(Projektas).filter(
     Projektas.price > 10000,
